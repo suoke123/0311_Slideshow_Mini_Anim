54666e16af3b666d00e8599e0db7e55bb13324d2
suoke123 <asak222@hanmail.net>
2021-03-11 17:27:45 +0900

    0311_Slideshow_Mini_Anim

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    setInterval(sample, 1000);
+    function sample() {
+
+        $("#innerFrame").animate(
+            {"margin-left": "-200px"},
+            3000,
+            "swing",
+            function(){
+                $("span:first-child").insertAfter("span:last-child");
+                $("#innerFrame").css({"margin-left": "0"});
+            }
+        );
+
+    } // 커스텀 함수 = 사용자 정의 함수, 호출하기 전엔 실행안됨
+
+});
